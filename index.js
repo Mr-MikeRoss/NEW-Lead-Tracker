202d97c73450467aa27cea726f8f3400db2cec85
Mr-MikeRoss <MrMikeRoss1995@gmail.com>
2025-04-16 09:56:58 -0400

    Update index.js
    Updated some javascript.

---
+++ index.js
@@ -0,0 +1,11 @@
+//arrays
+let myLeads = []
+
+//Dom Interacter
+let inputBtn = document.getElementById("input-btn")
+let inputEl = document.getElementById("input-el")
+
+//Dom Event Listeners
+inputBtn.addEventListener("click", function() {
+    
+})
