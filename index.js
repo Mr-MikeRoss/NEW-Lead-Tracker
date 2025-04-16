7bd7125fb32c7f69e84e999e8308f06c6d4a848c
Mr-MikeRoss <MrMikeRoss1995@gmail.com>
2025-04-16 10:36:47 -0400

    Update index.js
    Small additions added additionally.

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 let myLeads = []
 
 //Dom Interacter
-let inputBtn = document.getElementById("input-btn")
-let inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
 
 //Dom Event Listeners
 inputBtn.addEventListener("click", function() {
-    
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
