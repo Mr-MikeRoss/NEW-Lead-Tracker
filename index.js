2e1c9264ff8fd3018ad28374142816d0ad278c5e
Mr-MikeRoss <MrMikeRoss1995@gmail.com>
2025-04-18 15:07:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const inputEl = document.getElementById("input-el")
 
 //Dom Event Listeners
 inputBtn.addEventListener("click", function() {
+    if (inputEl.value === myLeads[myLeads.length - 1]) {
+        return
+    } 
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
